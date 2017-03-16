32d4476bc71fafadaa78098483d9a450f0bf2d1e
jacobking <jacob.king@realpage.com>
2017-03-16 15:50:33 -0500

    Add Radio Component

---
+++ components/radio/index.js
@@ -0,0 +1,18 @@
+// css only for checkbox to work
+
+
+// this code just verifies the display controls the element
+// $('#one').on('click', function(e){
+//     $('#radio-one').trigger('click');
+// });
+// $('#two').on('click', function(e){
+//     $('#radio-two').trigger('click');
+// });
+//
+//
+// $('#one1').on('click', function(e){
+//     $('#radio-one').find('input').prop("checked", true);
+// });
+// $('#two2').on('click', function(e){
+//     $('#radio-two').find('input').prop("checked", true);
+// });
